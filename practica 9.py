f4be4b500300c75542382e8919d9c9854d77cdd9
ESME902 <riosolaldeesperanza@gmail.com>
2025-03-11 16:24:36 -0600

    Add files via upload

---
+++ practica 9.py
@@ -0,0 +1,7 @@
+frutas = ("manzanas, pera, naranja, papaya")
+colores =("azul, amarillo, violeta, gris")
+print("listas de frutas", frutas)
+print( "lista de colores",colores)
+Frutas.append("Sandia")
+frutas. append(higo
+print("lista de frutas", frutas)
